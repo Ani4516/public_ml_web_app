f06e85e3834940b6262279f9ab340529a3566160
Ani4516 <150329000+Ani4516@users.noreply.github.com>
2024-04-28 21:03:10 +0000

    err

--- diagonise_patient.py
+++ diagonise_patient.py
@@ -67,7 +67,7 @@ if(selected == 'Diabetes Prediction'):
         
         if (diab_prediction[0] == 1):
           diab_diagnosis = 'The person is diabetic'
-          st.error(diab_diagnosis);
+          st.error(diab_diagnosis)
         else:
           diab_diagnosis = 'The person is not diabetic'
           st.success(diab_diagnosis)
